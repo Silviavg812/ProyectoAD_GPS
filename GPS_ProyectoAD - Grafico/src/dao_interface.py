13939c1e6f7798ead23c9a78658c4c86f9a55065
Silvia Vargas <silviavargasguada@gmail.com>
2026-02-11 09:34:11 +0100

    s

--- GPS_ProyectoAD - Grafico/src/dao_interface.py
+++ GPS_ProyectoAD - Grafico/src/dao_interface.py
@@ -4,6 +4,5 @@ from src.graph import Edge, NodeId
 
 
 class GraphDAO(Protocol):
-    """Tu compañero implementa esto con SQLite. Aquí solo la interfaz."""
     def get_neighbors(self, node: NodeId) -> Iterable[Edge]:
         ...
